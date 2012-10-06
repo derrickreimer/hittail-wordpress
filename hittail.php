3088baef9f5aeab1cd18749c6b23471c911c5da3
Derrick Reimer <derrickreimer@gmail.com>
2012-10-06 11:22:15 -0700

    Add tracking code hook

--- hittail.php
+++ hittail.php
@@ -19,7 +19,7 @@ $ht_options = get_option('ht_options');
 // Hooks
 //===========================
 
-// Public: Install the plugin and set default options.
+// Internal: Install the plugin and set default options.
 //
 // Returns nothing.
 function ht_install() {
@@ -34,3 +34,24 @@ function ht_install() {
 
 register_activation_hook(__FILE__, 'ht_install');
 
+// Internal: Output the HitTail tracking code for the site id set
+// in the options.
+//
+// Returns a String.
+function ht_tracking_code() {
+	$id = $ht_options['site_id'];
+	
+	// Check if the ID is set and is an integer
+	if (isset($id) && strval(intval($id)) == strval($id)) {
+		echo '<!-- HitTail Code -->' .
+			'<script type="text/javascript">' .
+				'(function(){ var ht = document.createElement("script");ht.async = true;' .
+			  	'ht.type="text/javascript";ht.src="//' . $id . '.hittail.com/mlt.js";' .
+			  	'var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ht, s);})();' .
+			'</script>'
+	} else {
+		echo "<!-- HitTail: Please set your site id -->";
+	}
+}
+
+add_action('wp_footer', 'ht_tracking_code');
