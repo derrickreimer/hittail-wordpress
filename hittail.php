20527883a4f9376c27b9b122b557a3429eb0d50f
Derrick Reimer <derrickreimer@gmail.com>
2012-10-06 19:07:03 -0700

    Add admin page

--- hittail.php
+++ hittail.php
@@ -13,15 +13,20 @@ License: GPLv2
 // Globals
 //===========================
 
+define('HT_PLUGIN_BASENAME', plugin_basename(__FILE__));
 $ht_options = get_option('ht_options');
+
+//===========================
+// Includes
+//===========================
+
+require_once('lib/admin.php');
 
 //===========================
 // Hooks
 //===========================
 
-// Internal: Install the plugin and set default options.
-//
-// Returns nothing.
+// Install the plugin and set default options.
 function ht_install() {
 	global $ht_options;
 	
@@ -34,23 +39,22 @@ function ht_install() {
 
 register_activation_hook(__FILE__, 'ht_install');
 
-// Internal: Output the HitTail tracking code for the site id set
-// in the options.
-//
-// Returns a String.
+
+// Output the HitTail tracking code with the site ID set in the options.
 function ht_tracking_code() {
+	global $ht_options;
 	$id = $ht_options['site_id'];
 	
 	// Check if the ID is set and is an integer
-	if (isset($id) && strval(intval($id)) == strval($id)) {
+	if (isset($id) && $id != "") {
 		echo '<!-- HitTail Code -->' .
 			'<script type="text/javascript">' .
 				'(function(){ var ht = document.createElement("script");ht.async = true;' .
 			  	'ht.type="text/javascript";ht.src="//' . $id . '.hittail.com/mlt.js";' .
 			  	'var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ht, s);})();' .
-			'</script>'
+			'</script>';
 	} else {
-		echo "<!-- HitTail: Please set your site id -->";
+		echo '<!-- HitTail: Set your site ID to begin tracking -->';
 	}
 }
 
---
+++ lib/admin.php
@@ -0,0 +1,65 @@
+<?php
+
+//===========================
+// Hooks
+//===========================
+
+// Initialize plugin admin by registering settings
+function ht_register_settings() {
+	register_setting('ht_options_group', 'ht_options');
+	add_settings_section('ht_code_settings', 'Tracking Code Settings', 'ht_code_settings_text', 'hittail');
+	add_settings_field('ht_site_id', 'Site ID', 'ht_option_site_id', 'hittail', 'ht_code_settings');
+}
+
+add_action('admin_init', 'ht_register_settings');
+
+// Add plugin settings page to the WP dashboard
+function ht_admin_menu() {
+	add_options_page('HitTail Settings', 'HitTail', 'manage_options', 'hittail', 'ht_admin_settings');
+}
+
+add_action('admin_menu', 'ht_admin_menu');
+
+// Output the plugin settings page
+function ht_admin_settings() {
+	// Ensure the user has sufficient permissions
+	if ( !current_user_can( 'manage_options' ) )  {
+		wp_die( __( 'You do not have sufficient permissions to access this page.' ) );
+	} 
+	?>
+	
+	<div class="wrap">
+		<?php settings_errors(); ?>
+		<div id="icon-options-hittail" class="icon32"><br></div>
+		<h2>HitTail Settings</h2>
+		<form name="ht-settings-form" method="post" action="options.php">
+			<?php settings_fields('ht_options_group'); ?>
+			<?php do_settings_sections('hittail'); ?>
+			<p class="submit">
+				<input type="submit" name="submit" class="button-primary" value="Save Changes">
+			</p>
+		</form>
+	</div>
+	
+	<?php
+}
+
+// Add admin page stylesheet
+function ht_admin_styles() {
+	wp_enqueue_style('ht_admin_css', plugins_url('/css/hittail-admin.css', HT_PLUGIN_BASENAME));
+}
+
+add_action('admin_enqueue_scripts', 'ht_admin_styles');
+
+//===========================
+// Settings
+//===========================
+
+function ht_code_settings_text() {
+	echo '<p>Enter your site ID to enable your HitTail tracking code. Your site ID is located under Account &gt; Sites in your HitTail account.</p>';
+}
+
+function ht_option_site_id() {
+	global $ht_options;
+	echo "<input type='text' name='ht_options[site_id]' size='20' value='{$ht_options['site_id']}'>";
+}
