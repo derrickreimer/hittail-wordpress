39a76694337112c7efe6bf936766dfe672a01960
Derrick Reimer <derrickreimer@gmail.com>
2012-10-07 11:20:56 -0700

    Refactor into classes

--- hittail.php
+++ hittail.php
@@ -9,56 +9,66 @@ Author URI: http://www.hittail.com
 License: GPLv2
 */
 
-//===========================
-// Globals
-//===========================
-
 define( 'HT_PLUGIN_BASENAME', plugin_basename( __FILE__ ) );
-$ht_options = get_option( 'ht_options' );
-
-//===========================
-// Includes
-//===========================
 
 require_once( 'lib/admin.php' );
 
-//===========================
-// Hooks
-//===========================
-
-// Install the plugin and set default options.
-function ht_install() {
-	global $ht_options;
+class WP_HitTail {
+	protected $options;
 	
-	// Set default options
-	if ( ! isset( $ht_options['site_id'] ) ) { $ht_options['site_id'] = ""; }
-	if ( ! isset( $ht_options['is_disabled'] ) ) { $ht_options['is_disabled'] = 0; }
+	public function __construct() {
+		// Fetch options
+		$this->options = get_option( 'ht_options' );
+		
+		// Register install function
+		register_activation_hook( __FILE__, array( $this, 'install' ) );
+		
+		// Register hooks
+		$this->add_actions();
+	}
 	
-	// Save options
-	update_option( 'ht_options', $ht_options );
-}
+	// Install the plugin and set default options.
+	public function install() {
+		// Set default options
+		if ( ! isset( $this->options['site_id'] ) ) { $this->options['site_id'] = ""; }
 
-register_activation_hook( __FILE__, 'ht_install' );
-
-
-// Output the HitTail tracking code with the site ID set in the options.
-function ht_tracking_code() {
-	global $ht_options;
-	$id = $ht_options['site_id'];
+		// Save options
+		update_option( 'ht_options', $this->options );
+	}
 	
-	// Check if the ID is set and is an integer
-	if ( ! $ht_options['is_disabled'] ) {
-		if ( isset($id) && $id != "" ) {
-			echo '<!-- HitTail Code -->' .
-				'<script type="text/javascript">' .
-					'(function(){ var ht = document.createElement("script");ht.async = true;' .
-				  	'ht.type="text/javascript";ht.src="//' . $id . '.hittail.com/mlt.js";' .
-				  	'var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ht, s);})();' .
-				'</script>';
+	public function add_actions() {
+		// Place tracking code in the footer
+		add_action( 'wp_footer', array( $this, 'tracking_code' ) );
+	}
+	
+	// Lookup an option and return NULL unless it is set and non-empty
+	public function get_option( $key ) {
+		if ( isset( $this->options[ $key ] ) && $this->options[ $key ] != "" ) {
+			return $this->options[ $key ];
 		} else {
-			echo '<!-- HitTail: Set your site ID to begin tracking -->';
+			return NULL;
+		}
+	} 
+	
+	// Output the HitTail tracking code with the site ID set in the options.
+	public function tracking_code() {
+		// Check if the ID is set and is an integer
+		if ( ! $this->get_option( 'is_disabled' ) ) {
+			if ( $this->get_option( 'site_id' ) ) { ?>
+				
+				<!-- HitTail Code -->
+				<script type="text/javascript">
+					(function(){ var ht = document.createElement("script");ht.async = true;
+					ht.type="text/javascript";ht.src="//<?php echo $this->get_option( 'site_id' ) ?>.hittail.com/mlt.js";
+					var s = document.getElementsByTagName("script")[0]; s.parentNode.insertBefore(ht, s);})();
+				</script>
+				
+			<?php 
+			} else {
+				echo '<!-- HitTail: Set your site ID to begin tracking -->';
+			}
 		}
 	}
 }
 
-add_action( 'wp_footer', 'ht_tracking_code' );
+$wp_hittail = new WP_HitTail();
