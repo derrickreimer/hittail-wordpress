40cac821974685f84ce2724f418bf6c09a80111f
Derrick Reimer <derrickreimer@gmail.com>
2013-10-08 12:41:50 -0700

    Remove call to settings_errors()
    It looks like Wordpress now automatically outputs errors - this
    was causing duplicate messages to appear.

--- views/options.php
+++ views/options.php
@@ -1,5 +1,4 @@
 <div class="wrap">
-	<?php settings_errors(); ?>
 	<div id="icon-options-hittail" class="icon32"><br></div>
 	<h2>HitTail Settings</h2>
 	<form name="ht-settings-form" method="post" action="options.php">
